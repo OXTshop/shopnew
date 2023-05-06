15d06ba4a6385b852ba9ad607d1b2693ae68665e
GuilhermeTeo <hugh.laurie17@gmail.com>
2023-05-06 09:37:34 -0300

    {date.today}

---
+++ Deploy_git.py
@@ -0,0 +1,31 @@
+import pyautogui as paut
+import time
+import os
+from datetime import date
+
+
+data_hoje = date.today()
+print(data_hoje)
+paut.hotkey('win','r')
+paut.write('cmd', interval=0.1)
+paut.press('enter')
+time.sleep(0.9)
+
+paut.write("git init", interval=0.05)
+paut.press('enter')
+
+time.sleep(0.5)
+
+paut.write("cd C:/server_index_github", interval=0.05)
+paut.press('enter')
+paut.write("git init", interval=0.05)
+paut.press('enter')
+paut.write('git add .', interval=0.05)
+paut.press('enter')
+paut.write('git status', interval=0.05)
+paut.press('enter')
+time.sleep(1)
+
+paut.write('git commit -m "{date.today}"')
+paut.press('enter')
+
